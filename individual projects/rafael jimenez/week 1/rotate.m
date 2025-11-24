3df7ffbe5a8b407fa194e1a76a7d959c7ede5586
rajos29 <45465033+rajos29@users.noreply.github.com>
2025-11-23 20:28:40 -0600

    Delete individual projects/rafael jimenez/week 1 directory

--- individual projects/rafael jimenez/week 1/rotate.m
+++
@@ -1,9 +0,0 @@
-function v_rot = rotate(v, theta)
-
-    if ~isnumeric(v) || size(v,1) ~= 2
-        error('Input must be a 2×N matrix where each column is a 2D point');
-    end
-
-    R = [cos(theta), -sin(theta); sin(theta), cos(theta)];
-    v_rot = R * v;
-end
